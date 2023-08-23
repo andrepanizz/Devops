ad1a9650d15907d10dcb578d019d81375938c6df
Andre Panizza <andredevops7@gmail.com>
2023-08-23 11:22:37 -0300

    upgrade commit

---
+++ Alura-Devops/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+# Qual vai ser o provedor, que estamos utilizando para criar nossa infra-estrutura
+# O provedor tem algumas configurações:
+
+provider "aws" {
+    region = "us-west-2"
+}
+# agora abaixo passaremos informações sobre o recurso que iremos utilizar:
+resoutce "aws_instance" "app_server" {
+    ami         = "ami-08a52ddb321b32a8c"
+    instance_type = "t2-micro"
+    key_name = "iac-alura"
+}
