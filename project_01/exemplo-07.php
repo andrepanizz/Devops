571088a6f497f0716d47e41a7b04d49e03a7ea03
Andre Panizza <andredevops7@gmail.com>
2023-09-18 22:17:27 -0300

    update this code

--- project_01/exemplo-07.php
+++ project_01/exemplo-07.php
@@ -1,19 +1,32 @@
 <?php
-/** @param trabalhando com herança */
+
+/** @param trabalhando com herança
+ * exemplo pratico de utilização:
+*/
 
 class Documento {
-    private $numero;
 
-    public function getNumero(){
-        return $this->numero;
-    }
-    public function setNumero($n){
-        $this->numero = $n;
+private $numero;
 
-    }
+public function getNumero(){
+
+    return $this->numero;
 }
 
-class cpf extends Documento {
+public function setNumero($n){
+
+    $this->numero = $n;
+        
+    }
+
+}
+
+/** @param classe extendida below:
+ *
+ */
+
+class p1 extends Documento {
+    
     
 }
 
