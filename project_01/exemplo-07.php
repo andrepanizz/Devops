f4f250f0397dfce5aef5006cbb87aaf5a537d0d7
Andre Panizza <andredevops7@gmail.com>
2023-09-18 22:26:22 -0300

    update this code exemplo-07

--- project_01/exemplo-07.php
+++ project_01/exemplo-07.php
@@ -4,31 +4,35 @@
  * exemplo pratico de utilização:
 */
 
+
 class Documento {
 
-private $numero;
 
-public function getNumero(){
+private $numero; //atrubute
+
+public function getNumero(){ //method this recovery the value
 
     return $this->numero;
 }
 
-public function setNumero($n){
+public function setNumero($n){ // method this set a number
 
     $this->numero = $n;
         
     }
 
-}
+} // end the class Documento
 
-/** @param classe extendida below:
+/** @param classe extends this below:
  *
  */
 
 class p1 extends Documento {
     
     
-}
+} //end the class extends the P1 betwee Documento
+
+
 
 
 ?>
