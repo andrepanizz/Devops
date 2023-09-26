4599e1d05fdd35e06348cd76dda0b799bca2501b
Andre Panizza <andredevops7@gmail.com>
2023-09-26 08:25:04 -0300

    update this code

---
+++ project_01/exemplo-17.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Namespaces</title>
+</head>
+<body>
+    <?php
+    /** @param Aula sobre Namespaces */
+    // namespaces permitem agrupar classes dentro de diretorios, de forma organizada.
+    // 
+    ?>
+    
+</body>
+</html>
