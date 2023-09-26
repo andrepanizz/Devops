3042dd58500a34572f3b5f1b0c5ed207dd5ccd73
Andre Panizza <andredevops7@gmail.com>
2023-09-26 08:28:54 -0300

    update this code

---
+++ project_01/exemplo-18.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variaveis de Sessions</title>
+</head>
+<body>
+    <?php
+    /** @param Variaveis de Sessão: */
+    
+
+
+
+    ?>>
+   
+</body>
+</html>
