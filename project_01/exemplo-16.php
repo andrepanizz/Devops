d4b38592dc35f0a6e8b1ecbc7824f748d168d23c
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-09-25 22:48:17 -0300

    Update exemplo-16.php

--- project_01/exemplo-16.php
+++ project_01/exemplo-16.php
@@ -1,8 +1,31 @@
 <?php
-// exemplo utilizando a função spl_autoload_register();
-// exemplo utilizando a função spl_autoload_register();
+// exemplo utilizando a função spl_autoload_register(1° forma function normal);
+// exemplo utilizando a função spl_autoload_register(2° forma via função anonima);
 
 //Vamos criar uma pasta com a palavra chave abstratas:
+
+//Vamos criar uma pasta com a palavra chave abstratas:
+/** @param use uma função que nos auxilia que conseguimos registrar mais de uma função, como funções de auto-load;
+ * spl_autoload_register();
+ */
+
+ // vamos criar uma função pra incluir classes ()
+
+ function incluirClasse($nomeClasse){
+    if(file_exists($nomeClasse . ".php") === true){
+        require_once($nomeClasse);
+    }
+
+ }
+
+/** @param use Desta forma aqui fizemos dos dois jeitos, tanto declarando uma function e usando
+ * uma function anonima */ 
+spl_autoload_register("incluirClasse");
+spl_autoload_register(function($nomeClasse){
+    if(file_exists("abstratas".$nomeClasse.".php") === true){
+        require_once($nomeClasse.".php");
+    }
+});
 
 
 $carro = new DelRey();
