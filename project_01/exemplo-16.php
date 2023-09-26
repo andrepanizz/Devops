3996ca9510eb723bbcd8da58bef0a8a234cbd0a4
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-09-25 22:20:46 -0300

    Create exemplo-16.php

---
+++ project_01/exemplo-16.php
@@ -0,0 +1,13 @@
+<?php
+// exemplo utilizando a função spl_autoload_register();
+// exemplo utilizando a função spl_autoload_register();
+
+//Vamos criar uma pasta com a palavra chave abstratas:
+
+
+$carro = new DelRey();
+$carro->acelerar(80);
+
+
+
+?>
