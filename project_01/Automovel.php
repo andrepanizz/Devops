d28a3964bef145f214c3e70087da146d0a9f487b
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-09-25 21:42:34 -0300

    Update Automovel.php

--- project_01/Automovel.php
+++ project_01/Automovel.php
@@ -1,17 +1,23 @@
 <?php
+interface Veiculo {
+  public function acelerar($velocidade);
+  public function frear($velocidade);
+  public function trocarMarcha($marcha);
+}
+
 Abstract class Automovel implements Veiculo {
 
   public function acelerar($velocidade)
   {
-    echo ucfrist("O veiculo acelerou ate: " . $velocidade . "<b> km/h </b><br>");
+    echo ucfirst("O veiculo acelerou ate: " . $velocidade . "<b> km/h </b><br>");
   }
   public function frear($velocidade){
     
-    echo ucfrist("O veiculo frenou ate: " . $velocidade . "<b> km/h </b><br>");  
+    echo ucfirst("O veiculo frenou ate: " . $velocidade . "<b> km/h </b><br>");  
   }
   public function trocarMarcha($marcha)
   {
-     echo ucfrist("O veiculo engatou a marcha" . $marcha . "a uma velocidade de <b> 200 KM/h </b>");   
+     echo ucfirst("O veiculo engatou a marcha" . $marcha . "a uma velocidade de <b> 200 KM/h </b>");   
   }
   
 }
