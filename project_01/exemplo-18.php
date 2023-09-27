cf4a11c38dc31f84a0ad57f260b0c99b5cb94ed0
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-09-26 23:06:12 -0300

    Update exemplo-18.php

--- project_01/exemplo-18.php
+++ project_01/exemplo-18.php
@@ -19,7 +19,9 @@ echo $_SESSION["nome"];
 
 session_destroy(); // limpa a variavel e remove o usuario;
 
-echo "eu te amo meu amor vidinha";
+echo "eu te amo andre";
+echo "eu acho que quero estudar psicologia";
+
 
     ?>>
    
