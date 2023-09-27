fef1be2b7b581a29cc8649d8d379b3c09ee30018
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-09-27 15:51:28 -0300

    Update exemplo-01.php

--- project_01/mysqli/exemplo-01.php
+++ project_01/mysqli/exemplo-01.php
@@ -2,7 +2,7 @@
 // criar uma variavél de conexão:
 // Se fosse na nuvem, me forneceriam o IP ou o endereço deste servidor.
 
-$conn = new mysqli("localhost2","root","","dbphp7");
+$conn = new mysqli("localhost","root","","dbphp7");
 
 if($conn->connect_error) {
     echo "ERROR" . mysqli_connect_errno();
@@ -11,5 +11,21 @@ if($conn->connect_error) {
 
 }
 
+// inserindo um comando prepare:
+
+$stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES (?, ?)");
+$stmt->bind_param("ss",$login,$pass); //espera o tipo de dado da interogação(), e espera os valores que queriamos inserir nas colunas (deslogin, senha);
+
+$login = "user";
+$pass = "12345";
+
+$stmt->execute();
+
+$login = "root";
+$pass = "!@#$";
+
+$stmt->execute();
+
+# $stmt->bind_result();
 
 ?>
