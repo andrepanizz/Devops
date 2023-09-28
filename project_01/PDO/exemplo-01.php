7c21c08ad4fd53edc4fb8268f71ae4c8cf4cae0b
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-09-28 18:47:48 -0300

    Update exemplo-01.php

--- project_01/PDO/exemplo-01.php
+++ project_01/PDO/exemplo-01.php
@@ -1,7 +1,32 @@
 <?php
 //Ultilizando PDO para conexão;
+//PDO - PHP Data Object();
 
+//informamos o DSN - ou Data Search name;
+$conn  = new PDO("mysql:dbname=dbphp7;host=localhost", "root", ""); //o tipo de banco, o nome do banco, e onde ele esta, "usuario", "senha";
 
+// begin transaction = inicia uma transação, podemos trabalhar direto com transações;
 
+$stmt = $conn->prepare("SELECT * FROM tb_usuarios ORDER BY deslogin");
+$stmt->execute();
+$results = $stmt->fetchAll(pdo::FETCH_ASSOC); // vai fatiar diferente de fecth_array dentro do nosso while():
+    
+    /** @param fetch_array or fetch_assoc:
+     * 
+     * while($row = $result->fetch_array(MYSQLI_ASSOC)){
+     * 
+     * } */
+  
+var_dump($results);
+echo "<br><br>";
+
+// percorrendo:
+foreach($results as $row){
+
+    foreach($row as $key => $value) {
+        echo "<strong>" . $key . ":" . " " ." </strong>" . $value . "" . "<br>";
+    }
+ echo "-------------------------------------------------------------------------  <br>";
+}
 
 ?>
