d917acb79de7268c388a9887e89b6b2aa44348a5
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-09-28 22:03:10 -0300

    Update exemplo-03.php

--- project_01/PDO/exemplo-03.php
+++ project_01/PDO/exemplo-03.php
@@ -1,7 +1,17 @@
 <?php
+$conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
+$stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES (:LOGIN, :PASSWORD)");
+/** @param colocando um identificador daquele paramentro unico, 
+ *  vamos criar 02 variaveis de login e senha:
+ */
+$login = "jose";
+$password = "1234567890";
 
+$stmt->bindParam("LOGIN", $login);
+$stmt->bindParam("PASSWORD", $password);  
+// utilizamos o LOGIN e a Senha, passando as variaveis e os detalhes;
 
-
-
+$stmt->execute();
+echo "Inserido OK!";
 
 ?>
