93874eb624eb71057498b4605490b4c88afb1bbe
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-09-29 11:11:53 -0300

    Update exemplo-04.php

--- project_01/PDO/exemplo-04.php
+++ project_01/PDO/exemplo-04.php
@@ -1,10 +1,32 @@
 <?php
-/* Alterando dados em uma tabela no Banco de Dados */
+//Alterando dados no Banco de dados Mysql:
 
 $conn = new PDO("mysql:host=localhost;dbname=dbphp7","root","");
+if($conn == mysqli_connect_error() && mysqli_connect_errno()) {
+    throw new Exception(ucfirst("Não foi possivel conectar ao Banco de Dados Mysql" . mysqli_connect_error()));
+
+}else{
+    echo ucwords("<center><i><strong>A Conexão com Banco de dados foi estabelecida!</i><strong></center><br><br>");
+    echo "<hr>";
+
+}
 //utilizaremos aqui o comando UPDATE que altera dados de uma tabela. (tb_usuarios)
-$stmt = $conn->prepare(""); 
+$stmt = $conn->prepare("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID");
+
+// criando as variaveis com os valores para o bindParam;
+$login = "joao";
+$pass = "qwerty";
+$id = 2;
+// adicionando:
+$stmt->bindParam(":LOGIN",$login);
+$stmt->bindParam("PASSWORD",$pass);
+$stmt->bindParam("ID",$id);
+
+//executando:
 $stmt->execute();
-echo strtoupper("<strong> dados da tabela Alterados com sucesso! </strong>");
+
+// enviando uma mensagem na tela, com sleep:
+sleep(1);
+    echo strtoupper("<strong> dados da tabela Alterados com sucesso! </strong>");
 
 ?>
