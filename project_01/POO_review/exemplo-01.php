947a9bbf9cf19d12ef759ca3a3e7536cc38c3d9b
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-09-29 12:32:17 -0300

    Update exemplo-01.php

--- project_01/POO_review/exemplo-01.php
+++ project_01/POO_review/exemplo-01.php
@@ -1,11 +1,18 @@
 <?php
 /**
  * Fazendo o Review de POO:
- * Estas parte e importante para se familiarizar com o Data_Acess_Object;
- *  - exemplo - classe cliente, cadastro
+ * Esta parte e importante para se familiarizar com o Data_Acess_Object;
+ *  - exemplo - classe cliente, cadastro;
  *  - utilizaremos o extends, emcapsulamento: public, protected, private;
  *  - Na parte de Dao, foi criado uma tabela chamada tb_usuarios, poderiamos criar outras (clientes), campos: End. Documento. Cpf.
  *  -  Vamos adicionar mais parametros: verificaremos como e o INSERT, UPDATE, E O DELETE.
+ * variaveis - se chamaram Atributos -> Emcapsulados [public, protected, private]
+ * funções - se chamaram  Metodos    -> Os metodos podem ser chamados normalmente e podem ser emcapsulados [public, protected, private]
+ * classes - São assuntos que criamos para organizar -> ganhamos variaveis e funções que são chamadas de atributos e metodos;
+ * Objeto -  Instanciamento - Guando efetivamente vou usar esta classe -> o objeto pode executar os metodos ex: $object->acelerar();
+ * 
  */
+
+
 
 ?>
