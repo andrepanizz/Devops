f0dede073b736255ad17a9752c04dcc79c23a537
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-09-29 12:24:55 -0300

    Add files via upload

---
+++ project_01/POO_review/exemplo-01.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Fazendo o Review de POO:
+ * Estas parte e importante para se familiarizar com o Data_Acess_Object;
+ *  - exemplo - classe cliente, cadastro
+ *  - utilizaremos o extends, emcapsulamento: public, protected, private;
+ *  - Na parte de Dao, foi criado uma tabela chamada tb_usuarios, poderiamos criar outras (clientes), campos: End. Documento. Cpf.
+ *  -  Vamos adicionar mais parametros: verificaremos como e o INSERT, UPDATE, E O DELETE.
+ */
+
+
+
+
+
+?>
