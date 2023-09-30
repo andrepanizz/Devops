f9d5cc1a95f9a09382d1fe6a55b11243f9d1780e
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-09-30 14:42:53 -0300

    Update to_string_exemple-01.php

--- project_01/POO_review/to_string_exemple-01.php
+++ project_01/POO_review/to_string_exemple-01.php
@@ -1,7 +1,20 @@
 <?php
 //Exemplo de To_string  varios exemplos;
-
-
+class Hello {
+    private $nome;
+ 
+    public function  __construct($nome) {
+        $this->nome = $nome;
+    }
+ 
+    public function  __toString() {
+        return 'Hello ' . $this->nome;
+    }
+}
+ 
+$class = new Hello('Rafael Wendel Pinheiro');
+echo $class;
+//Escreverá "Hello Rafael Wendel Pinheiro"
 
 
 ?>
