b3e747ece647b444890a7db16a46632d5709ddc9
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-09-30 15:01:37 -0300

    Create exemplo-02.php

---
+++ project_01/POO_review/exemplo-02.php
@@ -0,0 +1,15 @@
+<?php
+//exemplo da um metodo magico to__string
+
+class Market {
+    protected static $products;      // product the market actives
+    protected static $cnpj;          // National Register of Legal Entities
+    protected static $funds_cash;    // funds and cash
+    protected static $property_name; // property name
+
+    //getters and setters
+}
+
+
+
+?>
