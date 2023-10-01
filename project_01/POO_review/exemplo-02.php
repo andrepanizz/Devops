5856b88ffb98af7a5e2182f3443509a82e4747a8
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-09-30 21:54:26 -0300

    Update exemplo-02.php

--- project_01/POO_review/exemplo-02.php
+++ project_01/POO_review/exemplo-02.php
@@ -1,15 +1,103 @@
 <?php
-//exemplo da um metodo magico to__string
+class define {
+    
+    protected string $a; 
+    protected string $b; 
 
-class Market {
-    protected static $products;      // product the market actives
-    protected static $cnpj;          // National Register of Legal Entities
-    protected static $funds_cash;    // funds and cash
-    protected static $property_name; // property name
+    public function __construct($value1,$value2)
+    {
+        $this->a = $value1;
+        $this->b = $value2;
+                
+    }
+    //getters:
 
-    //getters and setters
+    public function getLetra_A():string
+    {
+        return $this->a;
+    }
+    public function getLetra_B():string
+    {
+        return $this->b;
+    }
+
+    public function __toString()
+    {
+        return ucfirst(" <strong> Maridinho Nenem: </strong>").$this->a . "<br> " .ucfirst("<strong> Esposinha Linda: </strong>") .  $this->b . "<br><br>";
+        
+
+    }
+
+    public function definition()
+    {
+        $as = array("som","carro");
+        array_push($as, "rodas","vidros", "Portas");
+
+        $arrs = get_defined_vars();
+
+        foreach($arrs['as'] as $key => $value){
+            echo " " . $key . ": " . $value . "<br>";
+            echo "<hr>";
+        }
+
+        
+
+        //print_r($arrs['as']) . "<br>";
+    }
+
+    public function filhos()
+    {
+        echo ucwords("Melissa Rodrigues") . "<br>";
+        echo ucwords("Rebecca Rodrigues") . "<br>";
+
+    }
+
+
 }
 
+class andre extends define {
+public function view()
+{
+    global $objeto;
+    return array("Esposa querida" => $this->b,
+                 "Marido Maravilhoso" => $this->a,
+                "Filhos: " => $objeto->filhos()
+            );
+}
 
+}
+
+$objeto = new define("andre", "erica");
+$objeto2 = new andre("andre", "erica");
+//echo $objeto . "<br>"; //toString
+//print_r($objeto2->getLetra_A()) . "<br>";
+//echo "<br>";
+//print_r($objeto2->getLetra_B()) . "<br>";
+//echo "<br>";
+echo "<hr>";
+print_r($objeto2->definition()) . "<br>";
+echo "<br>";
+print_r($objeto2->view());
+echo "<br>";
+
+echo "<br>"; 
+echo "<hr>";
+
+
+/*
+$a = array(1,2,3,4,5,6,7,8,9,10);
+$arr = get_defined_vars();
+print_r($arr['a']) . "<br>";
+echo "<hr>";
+
+$n1 = "produto";
+$n2 = "funds_cash";
+$n3 = "property_name";
+
+$f = array($n1,$n2,$n3);
+$andre = get_defined_vars();
+print_r($andre['f']) . "<br>";
+echo "<hr>";
+*/
 
 ?>
