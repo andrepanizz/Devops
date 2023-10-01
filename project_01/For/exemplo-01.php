72b3a0893e0ceef42b75d1f575df02c1413cac41
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-10-01 12:00:25 -0300

    Update exemplo-01.php

--- project_01/For/exemplo-01.php
+++ project_01/For/exemplo-01.php
@@ -1,5 +1,13 @@
 <?php
+/*E dividido em 03 partes
+1º Iniciar nosso contador:
+2º O for vai funcionar até que $i for menor que 10 por exemplo
+3º Utilizamos o Operador de incremento ou decremento ++ ou -- para ele ir percorrendo:
 
+*/
+for($i=0;$ < 10; $i++){
+  echo $i . "<br>";
+}
 
 
 
