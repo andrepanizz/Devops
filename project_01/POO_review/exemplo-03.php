c60c63f241034d1617b997666851dc9cd37d8065
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-09-30 22:09:18 -0300

    Update exemplo-03.php

--- project_01/POO_review/exemplo-03.php
+++ project_01/POO_review/exemplo-03.php
@@ -1,5 +1,19 @@
 <?php
 // entendendo Metodos Estaticos em POO:
+Class Documento {
+  private string $numero;
 
+  public function getNumero()
+    {
+      return $this->numero;
+    }
+  public function setNumero($numero)
+    {
+      $this->numero = $numero;
+    }
+
+}
+  
+$objeto = new Documento();
 
 ?>
