e7a922042ba2960138f17b477ec3cbe60f0c5c36
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-10-01 12:28:02 -0300

    Update exemplo-01.php

---
+++ project_01/For/exemplo-01.php
@@ -0,0 +1,11 @@
+<?php
+//1º condição, a primeira vai criar um contador, 
+//2º O for vai funcionar enquanto o $i for menor que 10;
+//3º Somaremos de 1 em 1 ++ caso eu queira mais de 1 +=(e o valor)
+for($i=0; $i<10; $i++)
+{
+  // O que vc quer que repita no caso:
+  echo $i . "<br>";
+}
+
+?>
