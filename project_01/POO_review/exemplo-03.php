b222a80f4fe0320c62ee406b03e1ab844dc30d64
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-10-03 12:28:31 -0300

    Update exemplo-03.php

--- project_01/POO_review/exemplo-03.php
+++ project_01/POO_review/exemplo-03.php
@@ -1,71 +1,95 @@
 <?php
 // entendendo Metodos Estaticos em POO:
 Class Documento {
+  protected string $msg = "Não e Possivel Validar este CPF!";
   private string $numero;
 
-  public function getNumero()
+  public function getNumero():int
     {
       return $this->numero;
     }
+
+    public function retMsg()
+    {
+  
+  $aviso = strval($this->msg);
+  return $aviso . "<br>";
+    }
+
   public function setNumero($numero)
     {
+      /* if($resultado == false):
+          return throw new Exception();
+      endif;
+      try {
+        $resultado == NULL;
+      }catch (Exception $e) {
+        echo 'Exceção capturada: ', $e->getMessage(), "\n";
+      }*/
+
+      //quando formos setar um novo numero de cpf:
+      $resultado = Documento::validarCpf($numero);
+        if($resultado == false):
+                   
+          return throw new Exception(self::retMsg());
+            
+        endif;
+      
       $this->numero = $numero;
     }
+// function que retorna uma função para uma msg de erro personalizada:
 
-public static function calcVerify($number){
-      for($t = 9; $t < 11; $t++){
-        
-      }
-}
+// Aqui vai o metodo magico de validação do cpf:
+public static function validarCpf($cpf):bool{
 
-//Aqui vai o Method Static de Validação do cpf:
-public static function validarCpf($cpf){
-  //verifica se o numero o foi informado:
+  //verfica se um numero foi informado:
     if(empty($cpf)) return false;
-  //elimina possivel mascara:
-  $cpf = preg_replace('[^0-9]', '', $cpf);
-  $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
-  // verifica se o numero de digitos informados e igual a 11.
-    if(strlen($cpf) != 11){
+  //Elimina possivel Mascara:
+  $cpf = preg_replace('[^0-9]','',$cpf); 
+  $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT); 
+  //Verifica se o numero que digitamos informado e igual a 11
+    if(strlen($cpf) != 11) { //se o tamanho da variavel $cpf for diferente de 11 retorna falso pra mim
       return false;
-      /* Verifica se nenhuma das sequencias invalidas abaixo
-         foi digitada. caso afirmativo, retorna falso!*/
-    }else if($cpf == '00000000000' ||
-         $cpf == '11111111111'||
-         $cpf == '22222222222'||
-         $cpf == '33333333333'||
-         $cpf == '44444444444'||
-         $cpf == '55555555555'||
-         $cpf == '66666666666'||
-         $cpf == '77777777777'||
-         $cpf == '88888888888'||
-         $cpf == '99999999999'||
-         $cpf == 'xxxxxxxxxxx'){
-         return false;
+      //Verifica se nenhuma das sequencias invalidas abaixo
+    //foi digitada . Caso afirmativo . retorna false;
 
-    }else{
-      //Calcula os digitos verificadores para verificar se o 
-      //Cpf e valido:
+    }else if ($cpf == '00000000000' ||
+              $cpf == '11111111111'||
+              $cpf == '22222222222'||
+              $cpf == '33333333333'||
+              $cpf == '44444444444'||
+              $cpf == '55555555555'||
+              $cpf == '66666666666'||
+              $cpf == '77777777777'||
+              $cpf == '88888888888'||
+              $cpf == '99999999999') {
+                return false;
+      //Caucula quantos digitos verificadores para verificar  se o
+      //Cpf é valido:
+      }else{
+         /** @param $t inicializa com 09, enquanto $t for menor que 11, Incrementa de 1 em 1 pra mim */
+        for($t = 9; $t < 11; $t++) {            
+            for($d = 0, $c = 0; $c < $t; $c++){
+              /** @param $d inicia com 0. $c inicia com 0 enquanto $c = 0; for menor que $t = 9; incrementa o $c de 1 em 1 */
+                $d + $d = $cpf[$c] * (($t + 1) - $c);
+                  //var_dump($d);
+              }
+              //fora do segundo FOR temos:
+                $d = ((10 * $d) % 11) % 10; //O valor de $d precedencia de operador 10 multiplicado por $d e o resto da divisão de 11 e 10
+              // Se o cpf com $c for diferente de $d; realizarei a conta pra saber qual o valor final de $d, retorna false pra mim
+              if($cpf[$c] != $d):
+                return false;
+              endif;
+           } //fim do loop do for;
 
-
-    }
-   /** @param Nota sobre a função strlen():
-    * Essa function retorna o valor de uma string
-    * a string a ter seu valor contado strlen($string)
-    * O valor da string e 0 caso ela seja vazia();
-    *
-    */
-  
-
-
-}
-
+       }
+  }
+        
 }
   
 $cpf = new Documento();
-$cpf->setNumero("2165486548518");
+$cpf->setNumero("12345678999");
 var_dump($cpf->getNumero());
-//ira me mostrar: string() "13476784894
 
 /*Nota sobre a function str_pad(string $string,int $length,string $pad_string = " ",int $pad_type = STR_PAD_RIGHT) 
 -----------------------------------------------------------------------------------------------------------------
