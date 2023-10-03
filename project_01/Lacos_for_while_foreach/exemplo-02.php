62d9f24f4a1e5e8f013e1227682f46e4df2254ec
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-10-02 23:16:05 -0300

    Update exemplo-02.php

--- project_01/Lacos_for_while_foreach/exemplo-02.php
+++ project_01/Lacos_for_while_foreach/exemplo-02.php
@@ -1,4 +1,4 @@
-!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
@@ -10,8 +10,16 @@
     <h2><center><strong><i> Exemplo 02 de Laço For </i></strong></center></h2>
     <br><br>
     <hr>
+
     <?php
-    /** @param Exemplo 02 de Laço de repetição utilizando o FOR */
+     /** @param Exemplo 02 de Laço de repetição utilizando o FOR */
+    // exemplo de um for com o looping infinito:
+        for($i = 0; $i < 0; $i--):
+            echo " " . $i . "<br>";
+        endfor;
+        //o apache vai dar Timeout;
+        
+        ?>
 
 
 
