b764550bafb4508e4142ae68e2936b4a8812dfee
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-10-04 13:14:32 -0300

    Update calculadora.js

--- Javascript/Calculadora-Projeto/scripts/calculadora.js
+++ Javascript/Calculadora-Projeto/scripts/calculadora.js
@@ -1,4 +1,4 @@
 let calculadora = new CalcController;
 // A variavel calculadora e igual a uma nova instancia Calc controller.
 //estamos instanciando esta classe;
-//
+////////////////////////////////////////////////////////////////////////
