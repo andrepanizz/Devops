a34541e2b0edf5bc2d5f7f9e420a1034450da1de
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-10-04 16:16:35 -0300

    Update exemplo-04.php

--- project_01/POO_review/exemplo-04.php
+++ project_01/POO_review/exemplo-04.php
@@ -1,7 +1,62 @@
 <?php
+// metodos magicos:
 
-//revisão sobre metodos Magicos:
+class Cadastro {
+   
+    private string $logradouro;
+    protected string $numero;
+    private string $cidade;
+    private string $estado;
+    private string $complemento;
+    private string $bairro;
+    private string $pais;
+    private   int  $cep;
+    
+    
+
+    //estaremos criando aqui um Metodo construtor:
+    public function __construct($log, $numero, $city, $compl, $state, $bair, $country, $postalcode)
+    {
+        $this->logradouro = $log;
+        $this->cidade = $city;
+        $this->estado = $state;
+        $this->complemento = $compl;
+        $this->bairro = $bair;
+        $this->pais = $country;
+        $this->cep = $postalcode;
+
+    }
+
+    public function __toString()
+    {
+        
+    }
+
+    
+    //Getters and Setters:
+    public function setNumberDocument($numero)
+    {
+        return $this->numero;
+
+    }
+
+    
+
+    public function getNumberDocument(){
+
+    }
 
 
+
+}
+
+class Validacao extends Cadastro {
+
+    public static function validarCpf($cpf)
+    {
+
+    }
+
+}
 
 ?>
