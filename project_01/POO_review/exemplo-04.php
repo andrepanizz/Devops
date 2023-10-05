1dc24c09adc224f554c1d66482e675fc026f3979
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-10-05 19:32:54 -0300

    Update exemplo-04.php

--- project_01/POO_review/exemplo-04.php
+++ project_01/POO_review/exemplo-04.php
@@ -35,8 +35,7 @@ abstract class Cadastro {
     {
         echo $this->logradouro . "<br>" . $this->cidade . "<br>" . $this->estado . "<br>" . $this->complemento . "<br>" . $this->bairro . "<br>" . $this->pais . "<br>" . $this->cep . "<br>";
         echo "<hr>";
-        ///////////////////////////////////
-
+        
         
     }
 
@@ -67,6 +66,47 @@ class Validacao extends Cadastro {
     public static function validarCpf($cpf)
     {
         //aqui vai a validação do cpf;
+     if(empty($cpf)) {
+        return false;
+    }
+ 
+    $cpf = preg_match('/[0-9]/', $cpf)?$cpf:0;
+
+    $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
+     
+    
+    if (strlen($cpf) != 11) {
+        echo "length";
+        return false;
+    }
+    
+    else if ($cpf == '00000000000' || 
+        $cpf == '11111111111' || 
+        $cpf == '22222222222' || 
+        $cpf == '33333333333' || 
+        $cpf == '44444444444' || 
+        $cpf == '55555555555' || 
+        $cpf == '66666666666' || 
+        $cpf == '77777777777' || 
+        $cpf == '88888888888' || 
+        $cpf == '99999999999') {
+        return false;
+
+     } else {   
+         
+        for ($t = 9; $t < 11; $t++) {
+             
+            for ($d = 0, $c = 0; $c < $t; $c++) {
+                $d += $cpf[$c] * (($t + 1) - $c);
+            }
+            $d = ((10 * $d) % 11) % 10;
+            if ($cpf[$c] != $d) {
+                return false;
+            }
+        }
+ 
+        return true;
+    }
 
     }
 
