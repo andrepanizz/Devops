ee7ed71eeb6509a9011d352d868928a2b58d9e63
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-10-06 13:16:51 -0300

    Delete Javascript/Calculadora-Projeto directory

--- Javascript/Calculadora-Projeto/scripts/calculadora.js
+++
@@ -1,5 +0,0 @@
-window.calculator = new CalcController();
-// A variavel calculadora e igual a uma nova instancia Calc controller.
-//estamos instanciando esta classe;
-////////////////////////////////////////////////////////////////////////
-// Guando declaramos Window fica Global;
