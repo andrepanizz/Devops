42e23398f06c60c6f1b725717091aa8a59afada2
Andre Panizza <andredevops7@gmail.com>
2023-10-06 20:39:09 -0300

    update this code

--- project_01/POO_review/exemplo-01.php
+++ project_01/POO_review/exemplo-01.php
@@ -5,7 +5,8 @@
  *  - exemplo - classe cliente, cadastro
  *  - utilizaremos o extends, emcapsulamento: public, protected, private;
  *  - Na parte de Dao, foi criado uma tabela chamada tb_usuarios, poderiamos criar outras (clientes), campos: End. Documento. Cpf.
- *  -  Vamos adicionar mais parametros: verificaremos como e o INSERT, UPDATE, E O DELETE.
+ *  - Vamos adicionar mais parametros: verificaremos como e o INSERT, UPDATE, E O DELETE.
+ *  - vamos fazer isto nesta session
  */
 class Cliente {
 
