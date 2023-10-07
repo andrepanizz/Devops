88bc75a21c522b47defe7a718c676bee597d15b3
Andre Panizza <andredevops7@gmail.com>
2023-10-07 11:19:31 -0300

    update try catch com PHP

---
+++ project_01/try-catch/exemplo-01.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo de tratamento de erros com Try-Catch</title>
+</head>
+<body>
+    <h2><center><i>Erros com Try Catch Exemplos</i></center></h2>
+    <br><br>
+    <hr>
+    <?php
+    /** @param Try exemplo de function com try catch */
+    function dividir($x, $y) {
+        if ($y == 0) {
+            throw new Exception('é uma divisão por zero.');
+        }
+        $resultado = $x / $y;
+        return $resultado;
+    };
+    
+    try {
+        echo dividir(5,0)."<br/>";
+    } catch (Exception $e) {
+        echo 'Exceção capturada: ',  $e->getMessage(), "\n";
+    }
+
+    //exemplo pratico da documentação do PHP:
+    
+    
+
+    ?>
+    
+</body>
+</html>
---
+++ project_01/try-catch/exemplo-02.php
@@ -0,0 +1,9 @@
+<?php
+/** Try catch com PHP: */
+
+
+
+
+
+
+?>
