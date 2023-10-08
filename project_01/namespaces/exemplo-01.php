182eb4a9a191b9e9ae0443777d641e455af03268
Andre Panizza <andredevops7@gmail.com>
2023-10-08 11:22:58 -0300

    update

--- project_01/namespaces/exemplo-01.php
+++ project_01/namespaces/exemplo-01.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo 01 de Namespaces </title>
+</head>
+<body>
+
 <?php
 //Exemplos de Namespaces e sua utilização:
 
@@ -6,3 +15,8 @@
 
 
 ?>
+    
+</body>
+</html>
+
+
--- project_01/namespaces/exemplo-02.php
+++ project_01/namespaces/exemplo-02.php
@@ -1,8 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Namespaces exemplo 02 </title>
+</head>
+<body>
 <?php
-//Exemplos de como utuilizar namespaces
+//Exemplos de como utuilizar namespaces:
 
 
 
 
 
 ?>
+</body>
+</html>
