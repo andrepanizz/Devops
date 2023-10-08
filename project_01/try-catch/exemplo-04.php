82c53053592a998e6fe804418de3f0ce196e56b1
Andre Panizza <andredevops7@gmail.com>
2023-10-08 11:23:57 -0300

    update

--- project_01/try-catch/exemplo-04.php
+++ project_01/try-catch/exemplo-04.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Try Catch exemplos</title>
+</head>
+<body>
+
 <?php
 //exemplo de try catch no site Manual do PHP
 /** Interação entre blocos */
@@ -44,3 +53,6 @@ echo "<hr>";
 
 
 ?>
+    
+</body>
+</html>
