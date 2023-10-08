f73dfcadd99999ca94d76b60d599b45f32fe07f6
Andre Panizza <andredevops7@gmail.com>
2023-10-08 11:16:15 -0300

    update

--- project_01/try-catch/exemplo-04.php
+++ project_01/try-catch/exemplo-04.php
@@ -1,7 +1,45 @@
 <?php
+//exemplo de try catch no site Manual do PHP
+/** Interação entre blocos */
 
+function test() {
+    try {
+        throw new Exception('foo');
+    } catch (Exception $e) {
+        return 'catch';
+    } finally {
+        return 'finally';
+    }
+}
 
+echo test(); 
 
+//////////////////////////////////////////////////////////////////
+echo "<br>";
+echo "Exemplos de Expressões alinhadas";
+echo "<hr>";
+
+class MyException extends Exception { }
+
+class Test {
+    public function testing() {
+        try {
+            try {
+                throw new MyException('foo!');
+            } catch (MyException $e) {
+                // rethrow it
+                throw $e;
+            }
+        } catch (Exception $e) {
+            var_dump($e->getMessage());
+        }
+    }
+}
+
+$foo = new Test;
+$foo->testing();
+
+echo "<hr>";
 
 
 
