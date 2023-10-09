230c8fb14106d5979508d959c40f74df081c66dd
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-10-09 12:38:10 -0300

    Update exemplo-05.php

--- project_01/POO_review/exemplo-05.php
+++ project_01/POO_review/exemplo-05.php
@@ -19,6 +19,8 @@
 
         public function verDados(){
             echo $this->nome . "<br/>";
+            echo $this->idade . "<br/">;
+            echo $this->senha . "<br/">;
         }
 
     }
