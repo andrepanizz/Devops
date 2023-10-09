a1b49763c1f237736e679f6bb78c460a597dc644
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-10-09 12:12:03 -0300

    Update exemplo-04.php

--- project_01/POO_review/exemplo-04.php
+++ project_01/POO_review/exemplo-04.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Metodos Magicos Exemplo 04 </title>
 </head>
 <body>
     <?php
@@ -12,6 +12,9 @@
 
 
 
+
+
 ?>
+<script src=""></script>
 </body>
 </html>
