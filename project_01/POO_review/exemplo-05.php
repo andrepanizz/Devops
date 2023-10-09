ecee4de6b9bd610452eabf4d5a7cfb266ceff1da
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-10-09 12:14:49 -0300

    Create exemplo-05.php

---
+++ project_01/POO_review/exemplo-05.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../logo/php.png">
+    <title> Exemplo de Emcapsulamento 02 </title>
+</head>
+<body>
+    <?php
+    // Exemplo de Emcapsulamento:
+
+    class Pessoa {
+        public $nome = "Ramus Lerdorf";
+        protected $idade = 48;
+        private $senha = "123456";
+
+        public function verDados(){
+            echo $this->nome . "<br/>";
+        }
+
+    }
+
+
+
+    ?>
+    
+</body>
+</html>
