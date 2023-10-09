1676335f31d74dfea0dbf19015f4808e2f3633e6
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-10-09 12:27:29 -0300

    Update exemplo-05.php

--- project_01/POO_review/exemplo-05.php
+++ project_01/POO_review/exemplo-05.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="../logo/php.png">
+    <link rel="stylesheet" type="text/css" href="/Alura-Devops/php7/css/style.css" media="screen"/> 
     <title> Exemplo de Emcapsulamento 02 </title>
 </head>
 <body>
