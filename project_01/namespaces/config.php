6ef4ea2cae66c1aaf77741d87a82998b4c062c9a
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-10-09 16:33:50 -0300

    Update config.php

--- project_01/namespaces/config.php
+++ project_01/namespaces/config.php
@@ -1,18 +1,13 @@
 <?php
-
-<?php
 //Configurando o projeto com um arquivo de configuração:
 spl_autoload_register(function($nameClass){
     //vamos falar para o php onde ele deve procurar as nossas classes;
     //criaremos uma variavel:
     $dirClass = "class";
     $filename = $dirClass . DIRECTORY_SEPARATOR .$nameClass.".php"; //normalmente se refere a todo nome do arquivo, a extenção dele e o PAFH
-    if(file_exists($filename) === true)) {   //a function file Existes retorna true ou false;
-        require_once("$filename");
+    if(file_exists($filename === true)){
+        require("$filename");
     }
+    //a function file Existes retorna true ou false;
 });
-
-?>
-
-
 ?>
