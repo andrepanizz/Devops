d63247e1816aed43f7429632bcc2170b9b6efcf2
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-10-09 16:29:56 -0300

    Update exemplo-01.php

--- project_01/namespaces/exemplo-01.php
+++ project_01/namespaces/exemplo-01.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 01 de Namespaces </title>
+    <title> Exemplo 01 de Namespaces </title>
 </head>
 <body>
+    <h2>Trabalhando com Namespaces</h2>
+    <br>
 
 <?php
 //Exemplos de Namespaces e sua utilização:
