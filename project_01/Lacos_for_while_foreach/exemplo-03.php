7f9a35dfdf8cd4416179977a497724fdb7c2fbad
Andre Panizza <andredevops7@gmail.com>
2023-10-10 17:38:56 -0300

    update

--- project_01/Lacos_for_while_foreach/exemplo-03.php
+++ project_01/Lacos_for_while_foreach/exemplo-03.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../logo/php.png"/>
+    <link rel="stylesheet" type="text/css" href="../css/style.css" media="screen"/> 
     <title>Exemplo de Laço While </title>
 </head>
 <body>
@@ -11,20 +13,7 @@
     <br><br>
     <hr>
     <?php
-    /** @param Exemplo 02 de Laço While */
-    <!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo de Laço While </title>
-</head>
-<body>
-    <h2><center><strong><i> Exemplo de Laço While e For </i></strong></center></h2>
-    <br><br>
-    <hr>
-    <?php
+  
     /** @param Exemplo 02 de Laço While */
     
     //Neste exemplo pratico de laço:
