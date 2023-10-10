1e660949d945640b55d247904cd7e7c7ae9f7730
Andre Panizza <andredevops7@gmail.com>
2023-10-10 17:43:25 -0300

    update this code

--- project_01/namespaces/exemplo-04.php
+++ project_01/namespaces/exemplo-04.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../logo/php.png"/>
+    <link rel="stylesheet" type="text/css" href="../css/style.css" media="screen"/> 
     <title>Namespaces exemplo 04</title>
 </head>
 <body>
