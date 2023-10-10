5e2d8eb4ad53b812b5c511dbe4ecaf47944f01cf
Andre Panizza <andredevops7@gmail.com>
2023-10-10 17:42:10 -0300

    update this code

--- project_01/mysqli/exemplo-02.php
+++ project_01/mysqli/exemplo-02.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../logo/php.png"/>
+    <link rel="stylesheet" type="text/css" href="../css/style.css" media="screen"/> 
+    <title>Mysqli_connected()</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+
 <?php
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
