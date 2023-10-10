b415fc1d73e4d1107a15d1cbd72e2719bbc14c7e
Andre Panizza <andredevops7@gmail.com>
2023-10-10 17:41:12 -0300

    update this code

--- project_01/mysqli/exemplo-01.php
+++ project_01/mysqli/exemplo-01.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../logo/php.png"/>
+    <link rel="stylesheet" type="text/css" href="../css/style.css" media="screen"/> 
+    <title>Mysqli connected </title>
+</head>
+<body>
+    
+</body>
+</html>
+
 <?php
 // criar uma variavél de conexão:
 // Se fosse na nuvem, me forneceriam o IP ou o endereço deste servidor.
