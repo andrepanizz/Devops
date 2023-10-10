f6fac92b4ca74562cbd92a4561bbde82e0ee40b9
Andre Panizza <andredevops7@gmail.com>
2023-10-10 17:39:39 -0300

    update

--- project_01/Lacos_for_while_foreach/exemplo-05.php
+++ project_01/Lacos_for_while_foreach/exemplo-05.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../logo/php.png"/>
+    <link rel="stylesheet" type="text/css" href="../css/style.css" media="screen"/> 
     <title>Exemplo de Laço FOR </title>
 </head>
 <body>
