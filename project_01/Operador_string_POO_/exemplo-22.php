3d381199bcba6c47054a647b56a2bdba27b74c44
Andre Panizza <andredevops7@gmail.com>
2023-10-12 09:06:23 -0300

    update

--- project_01/Operador_string_POO_/exemplo-22.php
+++ project_01/Operador_string_POO_/exemplo-22.php
@@ -3,16 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<link rel="icon" href="../logo/php.png">
+    <title> Exemplo 22 em PHP </title>
 </head>
 <body>
 <?php
-
 //exemplo 22 em PHP:
-
-
-
-
 
 
 
