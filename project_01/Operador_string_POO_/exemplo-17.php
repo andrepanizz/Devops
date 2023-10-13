5877d51076ea7b809f03392cb224d2478e9c7821
Andre Panizza <andredevops7@gmail.com>
2023-10-13 14:15:13 -0300

    update

--- project_01/Operador_string_POO_/exemplo-17.php
+++ project_01/Operador_string_POO_/exemplo-17.php
@@ -6,6 +6,7 @@
     <link rel="icon" href="../css/style.css"/>
     <link rel="stylesheet" type="text/css" href="../css/style.css" media="screen"/> 
     <title> Namespaces </title>
+    <!--Exemplo de namespaces exemplo-17.php -->
 </head>
 <body>
     <h2><center> Utilizando Namespaces </center></h2>
--- project_01/Operador_string_POO_/exemplo-18.php
+++ project_01/Operador_string_POO_/exemplo-18.php
@@ -6,6 +6,7 @@
     <link rel="icon" href="../css/style.css"/>
     <link rel="stylesheet" type="text/css" href="../css/style.css" media="screen"/> 
     <title>Variaveis de Sessions</title>
+    <!--Exemplos de Variaveis de Sessions[] = "text"; -->
 </head>
 <body>
     <h2><center> Variaveis de Sessions </center></h2>
