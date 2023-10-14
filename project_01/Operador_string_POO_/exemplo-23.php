12e9df04680c2192e54553c1fdd0a445114263e2
Andre Panizza <andredevops7@gmail.com>
2023-10-14 08:25:48 -0300

    update

---
+++ project_01/Operador_string_POO_/exemplo-23.php
@@ -0,0 +1,23 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" href="../logo/php.png">
+<link rel="stylesheet" type="text/css" href="../css/style.css" media="screen"/> 
+    <title> Exemplo 23 em PHP </title>
+<h2><center>Functions anonimas em PHP </center></h2>
+<br><br>
+</head>
+<!-- Exemplo 23 em PHP -->
+<body>
+<?php
+
+//Exemplo de function anoniams em PHP
+
+
+
+
+
+?>
+<script src=""></script>    
+</body>
+</html>
