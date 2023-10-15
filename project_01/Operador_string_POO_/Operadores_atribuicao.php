c0db3ddcc695d8eee690f805d87cbe3d6d0ded07
Andre Panizza <andredevops7@gmail.com>
2023-10-15 17:51:47 -0300

    update

--- project_01/Operador_string_POO_/Operadores_atribuicao.php
+++ project_01/Operador_string_POO_/Operadores_atribuicao.php
@@ -9,7 +9,7 @@
 <?php
 # Operadores de Atribuição:
 $nome = "Hcode"; # Operador de atribuição
-$concatenacao = "Operador de concatenação"; . $nome;
+$concatenacao = "Operador de concatenação" . $nome;
 $nome.= "Treinamentos"; #Eu to falando que minha variavel vai ter o valor "Hcode" e mais alguma coisa.
 echo $nome;
 ?>
