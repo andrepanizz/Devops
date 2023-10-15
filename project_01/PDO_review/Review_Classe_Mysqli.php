f12dab262012b257e12e8c6c0e1c21bd242ac572
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-10-15 14:48:25 -0300

    Update Review_Classe_Mysqli.php

--- project_01/PDO_review/Review_Classe_Mysqli.php
+++ project_01/PDO_review/Review_Classe_Mysqli.php
@@ -1,6 +1,31 @@
 <?php
 /*Review da Classe Mysqli no PHP7: */
 
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Conections with class (mysqli) </title>
+</head>
+<body>
+<?php
+//PDO() conexão com Mysqli_connect();
+//revisando:
+
+class connections {
+    //atributos e Metodos construct();
+    
+
+}
+
+$conn = new Mysqli(""); 
+
+?>
+    
+</body>
+</html>
 
 
 
