c5c19de0d16c6dd6100ec755ba707c7af68c7ae2
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-10-15 16:07:49 -0300

    Update exemplo-01.php

--- project_01/PDO_review/exemplo-01.php
+++ project_01/PDO_review/exemplo-01.php
@@ -6,8 +6,10 @@ if($conn->connect_error){
     echo ucwords("Error: ") . $conn->connect_errno;
     exit();
 }
-
+$stmt = $conn->query("");
 $stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES(? , ?)");
+
+
 $stmt->bind_param("ss","user","12456");
 $stmt->execute();
 
