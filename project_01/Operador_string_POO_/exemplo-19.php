13f6ade870721bc56d19aba21f00928d966011e5
Andre Panizza <andredevops7@gmail.com>
2023-10-16 08:55:06 -0300

    update

--- project_01/Operador_string_POO_/exemplo-19.php
+++ project_01/Operador_string_POO_/exemplo-19.php
@@ -9,18 +9,19 @@
     <!-- Variaveis de Session -->
 </head>
 <body>
-    
-</body>
-</html>
+    <h2> configuração do config.php </h2>
 <?php
 /*ID de Session*/
 /* Identificação da sua sessão unica no servidor */
 require_once("config.php");
 
 echo session_id();
-
+echo session_unset();
 ?>
 
 <script src=""></script>
+</body>
+</html>
+    
 </body>
 </html>
