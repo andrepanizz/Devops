f938492a1706425d69315adab7db0eec00de5b76
Andre Panizza <andredevops7@gmail.com>
2023-10-16 08:55:37 -0300

    update

--- project_01/Operador_string_POO_/exemplo-20.php
+++ project_01/Operador_string_POO_/exemplo-20.php
@@ -10,19 +10,21 @@
 </head>
 <body>
     <br>
-    
-</body>
-</html>
-
-
-<?php
+    <hr>
+    <?php
 require_once("config.php");
 
 session_regenerate_id();
 
 $id = ucfirst("O seu novo ID aleatorio de Session e: ") .  session_id() . "<br>";
 echo $id;
-var_dump($_SESSIION); // mostra o valor de array 
+var_dump($_SESSIION); // mostra o valor de array.. 
 
 
 ?>
+
+    
+</body>
+</html>
+
+
