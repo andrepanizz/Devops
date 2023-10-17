2d0afeda8e995e074b63491d80e3a282d6ab59a2
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-10-17 19:22:18 -0300

    Update Strings.php

--- project_01/Operador_string_POO_/Strings.php
+++ project_01/Operador_string_POO_/Strings.php
@@ -5,13 +5,17 @@ Diferença entre aspas simples pra aspas duplas:
 */
 $nome1 = "Hcode";
 $nome2 = 'Treinamentos';
+$nome3 = "Hcode" . 'Treinamentos' . "S/A";
 
 var_dump($nome1, $nome2);
+echo "<br>";
+var_dump($nome3);
+echo "<br>";
+
 /*
 A diferença: se estiver usando aspas duplas:
 echo "ABC $nome1";
-
-
+//////////////////////////////////////////////////////////////////////////////
 */
 
 
