665a9143921cb7941b759b28015855fc13e641ce
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-10-19 11:17:35 -0300

    Update exemplo-01.php

--- project_01/PDO_review/exemplo-01.php
+++ project_01/PDO_review/exemplo-01.php
@@ -1,1 +1,24 @@
+<?php
+# variavais:
+$hostname = "localhost";
+$usuario = "root";
+$password = "";
+$dbname = "dbphp7";
+$port = "3306";
+$socket = NULL;
 
+$conn = new mysqli($hostname, $usuario, $password, $dbname, $port, $socket);
+
+if($conn->connect_error):
+    echo strtoupper("Não foi Possivel conectar ao  Banco de Dados <br/>") . $conn->connect_errno;
+endif;
+echo "Conectado com Sucesso ao Banco de Dados!";
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+$stmt = $conn->prepare(""); //aqui faremos nossos Insert, Update, e Delete 
+// aqui faremos o Bind_Param();
+$stmt->bind_param("ss","?, ?");
+
+
+
+?>
