06e7bba09f3a2e6d7fece683fe818918e9f3e1eb
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-10-19 11:02:39 -0300

    Update exemplo-01.php

--- project_01/PDO_review/exemplo-01.php
+++ project_01/PDO_review/exemplo-01.php
@@ -8,11 +8,93 @@
 </head>
 <body>
     <?php
+    /*
+    
+Para se conectar ao banco de dados MySQL, precisamos saber 4 coisas.
+São elas:
+Host (servidor) - Lugar onde o banco de dados está.
+Usuário - Usuário para entrar no MySQL.
+Senha - Senha do MySQL.
+Database - A base de dados que criamos.
+  
+    
+    */
+    //esta function MySQL não fara nada além de instanciar o objeto em nossa pagina;
+     class MySQL {
+        //atributo:
 
-// A função Mysqli()
+        var $host = "localhost";
+        var $user = "root";
+        var $password = "";
+        var $database = "dbphp7";
+        var $query;
+        var $link;
+        var $result;
 
+      
+        /*
+        public function __destruct()
+        {
+            echo "destruiu o construtor";
+        }
+
+        */
+
+        function MySQL()
+        {
+            //Apenas instancia o Objeto;
+        }
+
+        // faremos agora a function para conexão com o banco de dados:
+        function connect()
+        {
+            
+            $this->link=mysqli_connect($this->host,$this->user,$this->password);
+            if(!$this->link){
+                echo strtoupper("Falha na Conexão com o banco de dados! <br>"). mysqli_connect_errno();
+                echo" ERRO!";
+                die();
+               
+             } elseif(!mysqli_select_db($this->database, $this->link)) {
+                echo "O banco de dados solicitado não pode ser aberto!<br>";
+                echo "Error" . mysqli_error();
+                die();
+
+             }
+        }
+
+        // esta function executa uma Query:
+        function executeQuery($query)
+        {
+            $this->connect();
+            $this->query=$query;
+            if($this->result=mysqli_query($this->query))
+            {
+                $this->disconnect();
+                return $this->result;
+
+            }else{
+                echo "Ocorreu um erro na execução SQL";
+                echo "Erro" . mysql_error();
+                echo "SQL" . $query;
+                die();
+                disconnect();
+
+            }   
+
+        }
+
+        // esta function desconecta do Banco de Dados;
+            function desconnect()
+            {
+                return mysqli_close($this->link);
+            }
+                 
+
+     }
 
 
 ?>
+
 </body>
 </html>
