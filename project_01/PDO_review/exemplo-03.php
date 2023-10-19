ad88c6c1f33e52bdd930c4678ace912277a000b8
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-10-19 15:10:14 -0300

    Update exemplo-03.php

--- project_01/PDO_review/exemplo-03.php
+++ project_01/PDO_review/exemplo-03.php
@@ -1,11 +1,22 @@
 <?php
+
 //conectando com PDO;
+$db = "mysql:dbname=dbphp7";
+$hostname = "host=localhost";
+$user = "root";
+$password = "";
+# preenchendo:
+$conn = new PDO("$db;$hostname,$user,$password");
 
-$conn = new PDO("");
+if(isset($conn)){
+    echo "<center> Conexão feita com o banco de Dados </center><br/>";
+    echo "<hr>";
+}
 
+$stmt = $conn->prepare("SELECT * FROM tb_usuarios ORDER BY deslogin");
 
+$stmt->execute();
 
+//conseguimos fazer o fetch_array();
+$stmt->fetchAll();
 
-
-
-?>
