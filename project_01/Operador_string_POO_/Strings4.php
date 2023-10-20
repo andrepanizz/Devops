22ef698b5b78b4cd231b8636a7467dee049c2ad0
Andre Panizza <andredevops7@gmail.com>
2023-10-20 10:32:28 -0300

    update

--- project_01/Operador_string_POO_/Strings4.php
+++ project_01/Operador_string_POO_/Strings4.php
@@ -1,5 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strings4</title>
+</head>
+<body>
+    <?php
+    echo "texto de string de exemplo para formatação";
 
-
-
-?>
+    ?>
+</body>
+</html>
