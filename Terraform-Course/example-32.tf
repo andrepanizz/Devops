7ddb5da18efc4642397fcc69f086368c21ea7b3d
Andre Panizza <andredevops7@gmail.com>
2023-10-21 17:37:08 -0300

    update

---
+++ Terraform-Course/example-32.tf
@@ -0,0 +1,9 @@
+# Souce data:
+
+data "aws_codeartifact_repository_endpoint" "test" {
+  domain     = aws_codeartifact_domain.test.domain
+  repository = aws_codeartifact_repository.test.repository
+  format     = "npm"
+}
+
+/////////////////////////////////////////////////////////////
