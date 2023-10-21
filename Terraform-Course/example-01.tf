05fb77bcaca71d2fe9a3ddad0f95adb91f294e3b
Andre Panizza <andredevops7@gmail.com>
2023-10-21 17:40:37 -0300

    update

--- Terraform-Course/example-01.tf
+++ Terraform-Course/example-01.tf
@@ -7,12 +7,14 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
+# Configure the AWS Provider:
 provider "aws" {
   region = "us-east-1"
 }
 
-# Create a VPC
+# Create a VPC:
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
 }
+/////////////////////////////////////////////////////////////////////////
+#Argument and references:
--- Terraform-Course/example-03.tf
+++ Terraform-Course/example-03.tf
@@ -1,7 +1,8 @@
 # Credentials can be provided by adding an access_key, secret_key, and optionally token, to the aws provider block.
-# usage:
+# Example the usage:
 provider "aws" {
   region     = "us-west-2"
   access_key = "my-access-key"
   secret_key = "my-secret-key"
 }
+///////////////////////////////////////////////////////////
