dbe14171164d74d00cc935078b5c283a7b7761de
Andre Panizza <andredevops7@gmail.com>
2023-10-22 13:44:25 -0300

    update

---
+++ project_01/PDO_review/examplo-04.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Getters and Setters and constructor</title>
+</head>
+<body>
+
+    <?php
+    //atributes:
+    
+
+
+
+
+    ?>
+    
+</body>
+</html>
