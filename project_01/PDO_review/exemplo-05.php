176254bd58e49b3246abf81962d444e7aea5fa5f
Andre Panizza <andredevops7@gmail.com>
2023-10-23 18:18:20 -0300

    update

--- project_01/PDO_review/exemplo-05.php
+++ project_01/PDO_review/exemplo-05.php
@@ -14,8 +14,38 @@
     <hr>
     <?php
     /** @param vamos começar criando nossa conexão: */
-    $conn = new PDO("");
-    
+    $conn = new PDO("mysql:host=localhost;dbname=dbphp7","root","");
+
+    $stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES (:LOGIN, :PASSWORD)");
+
+    $login = 'jose';
+    $password = '1234567890';
+    $dtcadastro = "cadastro exemplo";
+    $idusuario = "user";
+    //vamos fazer o Bind_param: para ligar os valores:
+    /*
+    :LOGIN = $login;
+    :PASSWORD = $password;
+     */
+    /* Importante para cada values que tivermos, devemos passar um bind_param para cada um deles;
+     se tivermos inserindo ooutros valores na tabela por exemplo: idusuarios, dtcadastro; e esses
+     valores não fossem de AUTO_INCREMENT devemos passar todos;
+    */
+    $stmt->bindParam(":LOGIN",$login);
+    $stmt->bindParam(":PASSWORD",$password);
+
+    //exemplo se tivermos mais parametros:
+
+    $stmt = $conn->prepare("INSERT INTO tb_usuarios (idusuario, dtcadastro, deslogin, dessenha) VALUES (:IDUSUARIO, :DTCADASTEO, :LOGIN, :PASSWORD)");
+    $stmt->bindParam(":IDUSUARIO",);
+    $stmt->bindParam(":DTCADASTRO",);
+    $stmt->bindParam(":LOGIN",$login);
+    $stmt->bindParam(":PASSWORD",$password);
+    //executando:
+    $stmt->execute();
+
+    echo "inserido OK!";
+    echo "<br>";
     ?>
     <script src="../JavaScript-Model/model.js"></script>
 </body>
