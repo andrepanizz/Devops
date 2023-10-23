f73f974f853d4d268f71880b029950aeec0bfed3
Andre Panizza <andredevops7@gmail.com>
2023-10-23 10:58:45 -0300

    update this code

---
+++ project_01/PDO_review/exemplo-05.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo 5 com Review.</title>
+    <link rel="icon" href="../logo/php.png">
+    <link rel="stylesheet" href="../PDO_review/css-exemplo-04.php/style-example4.css">
+</head>
+<body>
+    <!--PDO Inserindo dados no Mysql -->
+    <h2> Exemplo de Insert com o Banco </h2>
+    <br>
+    <hr>
+
+    <?php
+    
+
+
+    ?>
+</body>
+</html>
