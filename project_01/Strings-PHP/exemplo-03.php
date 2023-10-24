58529814615ef96e392a8738bef9849906f796b3
Andre Panizza <andredevops7@gmail.com>
2023-10-24 15:24:04 -0300

    update this code

--- project_01/Strings-PHP/exemplo-03.php
+++ project_01/Strings-PHP/exemplo-03.php
@@ -60,7 +60,22 @@ switch($idade) {
 
 // Agora vamos aprender a usar os Laços de Loop, While, Do While e for:
 
+// O while e simples e tranquilo, porem temos que tomar o maximo de cuidado pra não cair no infinito:
 
+    $contador = 0;
+
+while ($contador < 10) {
+  echo $contador;
+  $contador++; // imprimira 1, 2, 3, 4, 5, 6, 7, 8, 9     ele não imprimira o 10 pois não dizemos se e >= 10;
+}
+
+// note que temos um contador que inicializa em 0
+// depois temos enquanto o contador for menor que 10 mostre o contador na tela;
+// E por fim faça o incremento deste contador, a saida para este processo será:;
+
+//Como usar um Laço foreach sintaxe basica:
+
+    
 
 
 ?>
