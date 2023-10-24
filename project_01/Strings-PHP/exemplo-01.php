42aa56b1e45ca20d4e569c1cfadb90090ef34dd2
Andre Panizza <andredevops7@gmail.com>
2023-10-24 11:27:10 -0300

    update

--- project_01/Strings-PHP/exemplo-01.php
+++ project_01/Strings-PHP/exemplo-01.php
@@ -1,7 +1,35 @@
 <?php
 //strings em PHP formatadores:
-/////////////////////////////////////////
-    
+
+/*
+-> Funções uteis para arrays:
+
+
+*/
+//podemos acessos os elementos de um array desta forma:
+    $lista = ['a', 'b'];
+    $lista[0]; // 'a' indice começa em 0;
+    $lista[1];
+
+//Depois que o array e criado podemos indexar valores desta forma:
+    $lista1 = ['a','b'];
+    $lista1[] = 'c';
+
+/*
+$lista == [
+    "a",
+    "b",
+    "c",
+]
 
 ?>
+// Se quisermos adicionar itens no inicio do array podemos usar a function: array_unshift();
+*/
+$lista2 = ['b', 'c'];
+array_unshift($lista2, 'a');
+var_dump($lista2);
+echo "<br/>";
+echo "<hr>";
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
 
