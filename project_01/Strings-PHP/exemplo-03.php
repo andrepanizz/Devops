68cde65cb635b16fa370bbd1e2dd3ffa3ddb4112
Andre Panizza <andredevops7@gmail.com>
2023-10-25 11:13:53 -0300

    update

--- project_01/Strings-PHP/exemplo-03.php
+++ project_01/Strings-PHP/exemplo-03.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang = "pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+
+
 <?php
 /* Como funciona as condicionais no PHP:
 Com ela podemos decidir fazer uma coisa ou fazer outra,
@@ -287,3 +298,5 @@ class Gato {
 ?>
 
 
+</body>
+</html>
