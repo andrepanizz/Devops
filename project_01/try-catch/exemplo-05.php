d5ce4a8ad5ab61afd46b73663e6bc10a52a54ee8
Andre Panizza <andredevops7@gmail.com>
2023-10-26 08:39:09 -0300

    update

--- project_01/try-catch/exemplo-05.php
+++ project_01/try-catch/exemplo-05.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../logo/php.png">
     <title>Exemplo-05 com try catch</title>
 </head>
 <body>
@@ -10,6 +11,6 @@
     /** @param exemplo5: exemplos praticos  */
 
     ?>
-    
+<script src=""></script>    
 </body>
 </html>
