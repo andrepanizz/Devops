da1100272f2548ed4ae12f69e43a9b9b14e864de
Andre Panizza <andredevops7@gmail.com>
2023-10-26 08:36:44 -0300

    update

--- project_01/Strings-PHP/exemplo-01.php
+++ project_01/Strings-PHP/exemplo-01.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../logo/php.png">
     <title>Strings e functions PHP</title>
 </head>
 <body>
