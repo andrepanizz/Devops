c1dc6d023c28b2b18649198330213e0e993b81cc
Andre Panizza <andredevops7@gmail.com>
2023-10-26 08:35:36 -0300

    update

--- project_01/Strings-PHP/exemplo-02.php
+++ project_01/Strings-PHP/exemplo-02.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../logo/php.png">
     <title>Strings</title>
 </head>
 <body>
 <!-- Inicio do Arquivo -->
 <?php
-
 /* Exemplo de Strings:  */
 
 
---
+++ project_01/Strings-PHP/exemplo-05.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo de Strings em PHP - </title>
+    <link rel="icon" href="../logo/php.png">
+</head>
+<body>
+    <h2> Exemplo de Strings em PHP </h2>
+    <!-- exemplo de arquivo de Strings em PHP -->
+    <?php
+    // Exemplos de Strings em PHP:
+    // exemplo-04.php:
+
+    ?>
+    <!-- Fim do Arquivo -->
+<script src=""></script>    
+</body>
+</html>
