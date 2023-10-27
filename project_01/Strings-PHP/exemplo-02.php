5826838bdd3edb4070b3f92dc4b94f3f26da0a75
Andre Panizza <andredevops7@gmail.com>
2023-10-27 09:33:27 -0300

    update

--- project_01/Strings-PHP/exemplo-02.php
+++ project_01/Strings-PHP/exemplo-02.php
@@ -91,7 +91,33 @@ echo $user; //imprimira foo
 echo $pass; // * 
 
 /* Note sobre a function list:
+list — Cria variáveis como se fossem arrays:
 
+parametros:
+var:
+A variavel;
+
+vars:
+outras variaveis.
+
+retorna o array atribuido:
+$info = array('Café', 'marrom', 'cafeína');
+
+// Listando todas as variáveis
+list($bebida, $cor, $substancia) = $info;
+echo "$bebida é $cor e $substancia o faz especial.\n";
+
+// Listando apenas alguns deles
+list($bebida, , $substancia) = $info;
+echo "$bebida tem $substancia.\n";
+
+// Ou ignoramos os primeiros valores para conseguir apenas o último
+list( , , $substancia) = $info;
+echo "I need $substancia!\n";
+
+// list() não funciona com strings
+list($bar) = "abcde";
+var_dump($bar); // NULL
 */
 
 
