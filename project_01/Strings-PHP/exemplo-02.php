b93b6c6c75881731258a3e75cdbad18b69316681
Andre Panizza <andredevops7@gmail.com>
2023-10-27 16:30:46 -0300

    update

--- project_01/Strings-PHP/exemplo-02.php
+++ project_01/Strings-PHP/exemplo-02.php
@@ -123,6 +123,27 @@ echo str_pad($input, 10, "pp", STR_PAD_BOTH ); //ppinputppp;
 echo str_pad($input, 6, "p", STR_PAD_BOTH ); //inputp;
 echo str_pad($input, 8, "p", STR_PAD_BOTH ); // pinputpp;
 
+// stripos - O oposto de strpos();
+// exemplos de utilização:
+
+$findme    = 'a';
+$mystring1 = 'xyz';
+$mystring2 = 'ABC';
+
+$pos1 = stripos($mystring1, $findme);
+$pos2 = stripos($mystring2, $findme);
+
+// Nope, 'a' is certainly not in 'xyz'
+if ($pos1 === false):
+    echo "A String '$findme' Não foi encontrada em \string '$mystring1'";
+endif;
+
+// Note our use of ===.  Simply == would not work as expected
+// because the position of 'a' is the 0th (first) character.
+if ($pos2 !== false):
+    echo "foi encontrada '$findme' in '$mystring2' na posição $pos2";
+endif;
+
 
 // Strtoupper() Transforma uma String em Maiuscula:
 
