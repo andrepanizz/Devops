279df19b3a92f1b3da0bf8a6d30b02673d5c5880
Andre Panizza <andredevops7@gmail.com>
2023-10-27 08:15:34 -0300

    update

--- project_01/PDO_review/exemplo-06.php
+++ project_01/PDO_review/exemplo-06.php
@@ -11,6 +11,7 @@
 <?php
 // exemplos em PHP:
 
+
 ?>
 <script src=""></script>
 </body>
--- project_01/Strings-PHP/exemplo-02.php
+++ project_01/Strings-PHP/exemplo-02.php
@@ -10,6 +10,18 @@
 <!-- Inicio do Arquivo -->
 <?php
 /* Exemplo de Strings:  */
+/* ha muito mais functions internas que você pode usar para trabalhar com strings
+este e um pequena lista (não abrange) para mostrar algumas possibilidades:
+  TRIM Retira espaço no início e final de uma string
+*/
+
+// trim(: remove o espaço em branco no incio e no final um String.
+
+$text = "\t\teste e meu amigo! :)...  ";
+$binario = "\x09Exemplo de Strings";
+$ola = "Olá mundo";
+// mostrar as alterações com var_dump();
+
 
 
 
