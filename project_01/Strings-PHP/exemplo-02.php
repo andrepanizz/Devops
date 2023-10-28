51668205c98d6c3c92677fbdf8449a14871aa99c
Andre Panizza <andredevops7@gmail.com>
2023-10-28 14:50:50 -0300

    update code

--- project_01/Strings-PHP/exemplo-02.php
+++ project_01/Strings-PHP/exemplo-02.php
@@ -175,7 +175,8 @@ echo $arr[1];  //   baz;
 
 
 /* Nota Importante ou Important Notice:
- -> Como as variaveis no PHP não podem conter pontos ou
+ -> Como as variaveis no PHP não podem conter pontos ou espaços em seus nomes, estes são convertidos
+  em underlines.
 
 */
 
