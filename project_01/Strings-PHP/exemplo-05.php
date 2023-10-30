f147164dc5b4fb056f6d0315ab9f99d997b88d14
Andre Panizza <andredevops7@gmail.com>
2023-10-30 20:09:23 -0300

    update this code

--- project_01/Strings-PHP/exemplo-05.php
+++ project_01/Strings-PHP/exemplo-05.php
@@ -21,7 +21,15 @@
     */
 
     // is_array - Verifica se a variavel e um array();
-    
+
+    $garafa = array('Esta','e','uma','garafa');
+    echo is_array($garafa) ? 'Array' : 'Não e Array';
+    echo "\n";
+
+    $not_garafa = "Isto não e uma garafa";
+    echo is_array($not_garafa) ? 'Array' : 'Não e um Array';
+    echo "\n" . PHP_EOL;
+
 
     ?>
     <!-- Fim do Arquivo -->
