9312f7c989a4dd6f2b2997f63111748e9b9e1cfb
Andre Panizza <andredevops7@gmail.com>
2023-11-01 13:19:19 -0300

    update this code

---
+++ project_01/PDO_review_2/exemplo-01.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../logo/php.png"/>
+    <title> PDO_reviw </title>
+</head>
+<body>
+<?php
+//conexão com o banco de dados:
+$conn = new PDO(""); //
+
+
+?>
+    
+</body>
+</html>
