a063f67397b01dddf16cdf7ac702c55f8a1575e5
Andre Panizza <andredevops7@gmail.com>
2023-11-02 14:44:14 -0300

    update

--- project_01/PDO_review_2/file_02_dbtable2.php
+++ project_01/PDO_review_2/file_02_dbtable2.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exemplo-02 DAO - Data Acess Object</title>
+    <link rel="" href="../logo//php.png">
 </head>
 <body>
 <?php
