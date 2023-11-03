ad54b03b4eb5669ee26dc72f8abd35b1bce6fbfc
Andre Panizza dos Santos <141254971+andrepanizz@users.noreply.github.com>
2023-11-03 15:20:27 -0300

    Update file_01_dbtable1.php

--- project_01/PDO_review_2/file_01_dbtable1.php
+++ project_01/PDO_review_2/file_01_dbtable1.php
@@ -27,6 +27,9 @@
 //Connection:
 $conn = new PDO("mysql:host=localhost;dbname=dbphp7","root","");
 
+ $stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES ('user','gfdgs')");
+
+
 
 
 
